4aa71657079f1da656b9346e77897caee7b94a5d
AKSHAY <akshayjadhav461999@gmail.com>
2020-09-05 15:46:49 +0530

    passingprops , rendering list

---
+++ src/components/Todo.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>{{todoString}}</h1>
+</template>
+
+<script>
+export default {
+  props: {
+    todoString: String,
+    completed: Boolean,
+  },
+};
+</script>
+
+<style scoped>
+.completed {
+  text-decoration: line-through;
+}
+</style>
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,10 +1,77 @@
 <template>
-  <h1>Hello from TODO List</h1>
+  <div class="container">
+    <div class="row">
+      <div class="col-12">
+        <h1>Vue TODO List App</h1>
+      </div>
+    </div>
+    <div class="row">
+      <p class="display-3">Add Form</p>
+    </div>
+    <div class="row">
+      <div class="col-12">
+        <ul class="list-group">
+          <Todo
+            v-for="(todo,index) in todos"
+            :key="index"
+            :todoString="todo.todoString"
+            :completed="todo.completed"
+          />
+        </ul>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+import Todo from "./Todo.vue";
+export default {
+  components: {
+    Todo,
+  },
+  data() {
+    return {
+      todos: [
+        {
+          todoString: "Learn Vue",
+          completed: true,
+        },
+        {
+          todoString: "Learn UI",
+          completed: false,
+        },
+        {
+          todoString: "Make Tea",
+          completed: true,
+        },
+      ],
+    };
+  },
+  methods: {
+    addTodo(newTodo) {
+      this.todos.push({
+        todoString: newTodo,
+        completed: false,
+      });
+    },
+    toggleTodo(todo) {
+      todo.completed = !todo.completed;
+    },
+    editTodo(todo, editString) {
+      todo.todoString = editString;
+    },
+    deleteTodo(deleteTodo) {
+      this.todos = this.todos.filter(
+        (todo) => todo.todoString !== deleteTodo.todoString
+      );
+    },
+  },
+};
 </script>
 
 <style >
+body {
+  display: grid;
+  place-items: center;
+}
 </style>
